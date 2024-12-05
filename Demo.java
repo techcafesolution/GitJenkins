a14e1315e36de506996e5eaf66f5164f2b9a475f
Babul <sanjeebdash2050@gmail.com>
2024-12-05 08:28:37 +0530

    Adding some new line to Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,12 @@
 class Demo{
 	public static void main(String [] args){
 		
-		System.out.print("Hello World");
+		System.out.println("Hello World");
+		System.out.printf("The Sum is:: %d",sum(12,18); 
+	}
+
+	public static int sum(int a, int b){
+		
+		return a+b;
 	}
 }
