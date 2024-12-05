a41acf92e7668bd44cd3a5c22f9d413ebf03e6e6
Babul <sanjeebdash2050@gmail.com>
2024-12-05 08:31:25 +0530

    Add a paranthesis

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ class Demo{
 	public static void main(String [] args){
 		
 		System.out.println("Hello World");
-		System.out.printf("The Sum is:: %d",sum(12,18); 
+		System.out.printf("The Sum is:: %d",sum(12,18)); 
 	}
 
 	public static int sum(int a, int b){
